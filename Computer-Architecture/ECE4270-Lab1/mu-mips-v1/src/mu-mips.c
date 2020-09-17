5afeeac0e5075b1c2e04f42d6453fb99bb076a4e
Bailey Shepherd <baileyray8@aol.com>
2020-09-17 15:23:30 -0500

    jkdghfag

--- Computer-Architecture/ECE4270-Lab1/mu-mips-v1/src/mu-mips.c
+++ Computer-Architecture/ECE4270-Lab1/mu-mips-v1/src/mu-mips.c
@@ -865,6 +865,26 @@ void print_instruction(uint32_t addr){
 	/*IMPLEMENT THIS*/
 }
 
+
+
+
+uint32_t bit_flip(uint32_t v){
+	uint32_t r = v;         // r will be reversed bits of v; first get LSB of v
+	int s = sizeof(v) * CHAR_BIT - 1; // extra shift needed at end
+
+	for (v >>= 1; v; v >>= 1){   
+	    r <<= 1;
+	    r |= v & 1;
+	    s--;
+	 }
+	r <<= s;
+	return r;	// shift when v's highest bits are zero
+}
+
+
+
+
+
 /***************************************************************/
 /* main                                                                                                                                   */
 /***************************************************************/
